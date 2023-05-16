59b9c7f3e46ecc45e31d63e3779c1d86d9dcaf4f
assisnat <nas@ic.ufal.br>
2023-05-16 03:16:58 -0300

    Add a app icon, CTA and adjust local persistence in settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:medtrack/pages/confirm_qr_code_page.dart';
-import 'package:medtrack/services/alarms_service.dart';
-import 'package:medtrack/pages/home_page.dart';
-import 'package:medtrack/theme.dart';
-import 'package:alarm/alarm.dart';
+import 'pages/home_page.dart';
+import 'theme.dart';
 import 'package:medtrack/models/settings.dart';
 
 const settingsKey = 'user_prefs';
+Preference? settings;
 
-late Preference settings;
-
-const String chave = "medtrack qrCode";
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final now = DateTime.now();
-
+void main() async {
   await Hive.initFlutter();
   await Hive.openBox('medication');
   await Hive.openBox('alarm');
@@ -25,39 +15,28 @@ Future<void> main() async {
 
   final configBox = Hive.box('preference');
 
-  await Alarm.init(showDebugLogs: true);
-  
-  for (AlarmSettings alarm in Alarm.getAlarms()) {
-    if (alarm.dateTime.isBefore(now)) {
-      await Alarm.stop(alarm.id);
-    }
-  }
-
-  await Alarm.setNotificationOnAppKillContent(
-    'Seus alarmes podem não tocar', 
-    'Você fechou o app. Por favor, abra-o novamente para que os alarmes toquem.'
-  );
-
-  alarmsList = getAlarmList();
-  print("at main, alarmsList: $alarmsList");
-
   if (configBox.get(settingsKey) != null) {
-    settings = Preference.fromMap(configBox.get(settingsKey).cast);
+    settings =
+        Preference.fromMap(configBox.get(settingsKey).cast<String, dynamic>());
   } else {
     settings = Preference(
         key: settingsKey,
-        //selectedRingtones: ['sounds/mozart.mp3'],
-        receiveNotifications: true,
-        confirmAlarmQRCode: true,
-        receiveNotificationsTwentyMinutes: true);
+        selectedRingtone: 'sounds/mozart.mp3',
+        keyQRCode: '',
+        confirmAlarmQRCode: false,
+        receiveNotifications: true);
   }
 
   runApp(const MyApp());
 }
 
 extension StringCasingExtension on String {
-  String toCapitalized() => length > 0 ?'${this[0].toUpperCase()}${substring(1).toLowerCase()}':'';
-  String toTitleCase() => replaceAll(RegExp(' +'), ' ').split(' ').map((str) => str.toCapitalized()).join(' ');
+  String toCapitalized() =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
+      .split(' ')
+      .map((str) => str.toCapitalized())
+      .join(' ');
 }
 
 class MyApp extends StatelessWidget {
@@ -69,12 +48,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Home(),
-        '/scan/confirm': (context) => ScanQrCode()
-      },
-      // home: Home(),
+      home: const Home(),
     );
   }
 }
